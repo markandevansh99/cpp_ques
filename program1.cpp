869369d4001b9858aeaa4386cc2db5a5454e156e
markandevansh99 <500069794@stu.upes.ac.in>
2019-03-18 10:09:42 +0530

    Resolved error and added one line

--- program1.cpp
+++ program1.cpp
@@ -4,6 +4,10 @@ int main()
 {
 	 cout<<"Hello!!,This is the initial C++ program";
 	 //adding one more line
-	 cout<<"\nHello!!...This is the second line
+	 cout<<"\nHello!!...This is the second line";  //resolved error
+	 //so, adding one more line
+	 cout<<"This is mid sem exam";
+
+
 return 0;
 }
